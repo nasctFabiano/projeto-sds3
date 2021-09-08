3afcc5e82717b4d0831f986bdf34583a5db2e3b6
Fabiano Nascimento <zfabiano100@gmail.com>
2021-09-08 20:26:56 -0300

    Ajustes finais aula01

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,16 +12,16 @@ function App() {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
     <div className="row px-3">
-      <div className="col-sm-5">
+      <div className="col-sm-6">
         <h5 className="text-center" text-secondary>Taxa de sucesso (%)</h5>
         <BarChart />
       </div>
-    </div>
-    <div className="col-sm-6 left 30px">
+    
+    <div className="col-sm-6">
         <h5 className="text-center" text-secondary>Todas vendas</h5>
         <DonutChart />
       </div>
-
+      </div>
       <div className="py-3">
         <h2 className="text-primary">Todas as vendas</h2>
       </div>
